23dda39648293d9779985dce23fbb4f45b359a31
baygeldi <akylbekuulubaygeldi@gmail.com>
2022-11-17 18:47:17 +0600

    Initial Commit

---
+++ 1LessONUITests/_LessONUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _LessONUITestsLaunchTests.swift
+//  1LessONUITests
+//
+//  Created by Байгелди Акылбек уулу on 17/11/22.
+//
+
+import XCTest
+
+class _LessONUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
